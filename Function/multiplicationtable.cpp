c9f506481ba1de52d00ca714c4cd0977dd3042b0
s8080888 <s8080888@gmail.com>
2023-03-08 19:54:13 +0800

    新增自定義 9x9

--- Function/multiplicationtable.cpp
+++ Function/multiplicationtable.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 void NineNineMultiplicationTable();
+void NineNineMultiplicationTable(int[]);
 int main(int argc, char *argv[])
 {
-    NineNineMultiplicationTable();
+    int k[]{1, 4, 5, 6, 2, 3, 8, 7, 9};
+    NineNineMultiplicationTable(k);
 }
 void NineNineMultiplicationTable()
 {
@@ -12,6 +14,18 @@ void NineNineMultiplicationTable()
         for (int j = 1; j <= 9; ++j)
         {
             std::cout << i << " x " << j << " = " << i * j << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+void NineNineMultiplicationTable(int order[])
+{
+    int size = 9;
+    for(int i = 0;i < size; ++i)
+    {
+        for (int j = 0; j < size; ++j)
+        {
+            std::cout << (order[i]) << " x " << order[j] << " = " << order[i] * order[j] << " ";
         }
         std::cout << std::endl;
     }
